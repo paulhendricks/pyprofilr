954989363601977e966a78b11f1410230f804137
Paul Hendricks <paul.hendricks.2013@owu.edu>
2015-11-02 12:14:08 -0500

    changed setup.py settings

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
-__author__ = 'phendricks'
-from setuptools import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(name='pyprofilr',
       version='0.1',
       description='Profile data in Python',
+      long_description="""
+pyprofilr is a library for profiling data in Python 3.
+""",
       url='http://github.com/paulhendricks/pyprofilr',
       author='Paul Hendricks',
       author_email='paul.hendricks.2013@owu.edu',
+      maintainer='Paul Hendricks',
+      maintainer_email='paul.hendricks.2013@owu.edu',
       license='MIT',
       packages=['pyprofilr', 'pyprofilr.test'],
       zip_safe=False, 
